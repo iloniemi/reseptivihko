d11980a66b8668e4a94d505b4a186cefbaf88b2e
jumailon <juho.m.iloniemi@student.jyu.fi>
2020-02-27 21:03:13 +0200

    Resepti -luokka ja testit

--- reseptivihko/src/reseptivihko/Resepti.java
+++ reseptivihko/src/reseptivihko/Resepti.java
@@ -1,5 +1,8 @@
 package reseptivihko;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Resepti -luokka, joka pystyy asettamaan itselleen id:n.
  * @author Juho
@@ -7,24 +10,36 @@ package reseptivihko;
  *
  */
 public class Resepti {
-    private static int seuraavaId;
-    private int id;
-    private String nimi;
-    private String ohje;
-    
-    
-    
+    private static int SEURAAVA_ID = 0;
+    private int id = -1;
+    private String nimi = "";
+    private String ohje = "";
     
 
-    public Resepti(int id, String nimi, String ohje) {
-        this.id = id;
-        this.nimi = nimi;
-        this.ohje = ohje;
+    /**
+     * Parametritön konstruktori 
+     */
+    public Resepti() {
+        //
     }
     
     
-    public void setId(int id) {
-        this.id = id;
+    /**
+     * @return asetettu id
+     */
+    public int asetaId() {
+        if (this.id < 0) this.asetaId(SEURAAVA_ID);
+        return this.id;
+    }
+    
+    /**
+     * @param uusiId haluttu id
+     * @return asetettu id
+     */
+    private int asetaId(int uusiId) {
+        this.id = uusiId;
+        if (this.id >= SEURAAVA_ID) SEURAAVA_ID = this.id + 1;
+        return this.id;
     }
 
 
@@ -69,7 +84,28 @@ public class Resepti {
      */
     @Override
     public String toString() {
-        return String.format("ID: %d\n%s\n\n%s", this.id, this.nimi, this.ohje);
+        return String.format("ID: %d\n%s\n%s", this.id, this.nimi, this.ohje);
+    }
+    
+    /**
+     * Poimii merkkijonosta tiedot Reseptin attribuutteihin.
+     * Merkkijono muodossa "id|nimi|ohje§ohjeenSeuraavaRivi§seuraavaRivi".
+     * Palauttaa tehtiinkö parse onnistuneesti.
+     * @param merkkijono jäsenneltävä merkkijono
+     * @return onnistuiko rivin parsiminen
+     */
+    public boolean parse(String merkkijono) {
+        String regexReseptirivi = "^([0-9]+)\\|(.+)\\|(.*)$";
+        //1.id  2.nimi  3.ohje      |   1  |   | 2|   | 3|
+        Pattern kuvio = Pattern.compile(regexReseptirivi);
+        Matcher matcher =  kuvio.matcher(merkkijono);
+        
+        if (!matcher.matches()) return false;
+        
+        this.asetaId(Integer.parseInt(matcher.group(1)));
+        this.nimi = matcher.group(2);
+        this.ohje = matcher.group(3).replace('§', '\n');
+        return true;
     }
     
     /**
@@ -77,26 +113,25 @@ public class Resepti {
      * @param args ei kaytossa
      */
     public static void main(String[] args) {
-//        Resepti resepti = new Resepti();
-//        resepti.parsi("2|Suklaatryffelit|Kuumenna kerma kiehuvaksi.§Kaada kerma suklaan ja voin päälle.§Sekoita varovasti.§Mausta.§Muotoile käsissä.§Päällystä kaakaojauheella.");
-//        int reseptinId = resepti.getId();
-//        String reseptinNimi = resepti.getNimi();
-//        String reseptinOhje = resepti.getOhje();
-//        System.out.println("Reseptin id: " + reseptinId);
-//        System.out.println("Reseptin nimi: " + reseptinNimi);
-//        System.out.println("Reseptin ohje: " + reseptinOhje);
+        Resepti resepti = new Resepti();
+        resepti.parse("2|Suklaatryffelit|Kuumenna kerma kiehuvaksi.§Kaada kerma suklaan ja voin päälle.§Sekoita varovasti.§Mausta.§Muotoile käsissä.§Päällystä kaakaojauheella.");
+        int reseptinId = resepti.getId();
+        String reseptinNimi = resepti.getNimi();
+        String reseptinOhje = resepti.getOhje();
+        System.out.println("Reseptin id: " + reseptinId);
+        System.out.println("Reseptin nimi: " + reseptinNimi);
+        System.out.println("Reseptin ohje: " + reseptinOhje);
+        System.out.println(resepti.toString());
         
-        System.out.println();
-        
-//        Resepti resepti2 = new Resepti();
-//        resepti2.setNimi("Tee");
-//        resepti2.setOhje("Keita vesi.\nUita teepussia vedessa.");
-//        boolean onkoReseptilla2Id = resepti2.getId() != -1;
-//        System.out.println("Reseptilla2 on id: " + onkoReseptilla2Id);
-//        if (resepti2.hankiId()) System.out.println("Nyt on id.1");
-//        else System.out.println("Oli jo id.1");
-//        if (resepti2.hankiId()) System.out.println("Nyt on id.2");
-//        else System.out.println("Oli jo id.2");
-//        System.out.println(resepti2.toString());
+        Resepti resepti2 = new Resepti();
+        resepti2.setNimi("Tee");
+        resepti2.setOhje("Keita vesi.\nUita teepussia vedessa.");
+        boolean onkoReseptilla2Id = resepti2.getId() != -1;
+        System.out.println("Reseptilla2 on id: " + onkoReseptilla2Id);
+        if (resepti2.asetaId() > 0) System.out.println("Nyt on id.1");
+        else System.out.println("Oli jo id.1");
+        System.out.println(resepti2.toString());
     }
+
+        
 }
--- reseptivihko/src/reseptivihko/test/ReseptiTest.java
+++ reseptivihko/src/reseptivihko/test/ReseptiTest.java
@@ -4,15 +4,89 @@ import reseptivihko.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-class ReseptiTest {
+/**
+ * Testit Resepti -luokalle
+ * @author Juho
+ * @version 27 Feb 2020
+ *
+ */
+public class ReseptiTest {
 
+    /**
+     * Testaa parametritöntä konstruktoria
+     */
     @Test
-    public void test1() {
-        assertFalse(1>0);
+    public void testKonstruktoriIlmanParametreja() {
+        String tyhjaResepti = "ID: -1\n\n";
+        Resepti resepti = new Resepti();
+        assertEquals("Tyhja konstruktori ei luonut Reseptia oikein", tyhjaResepti, resepti.toString());
     }
+    
+    /**
+     * Testaa nimen asettamista
+     */
     @Test
-    public void testGetAstiat362() {    // AstiaPeli: 362
-        assertEquals("ei ollu sama", 1, 2);
+    public void testSetNimi() {
+        Resepti resepti = new Resepti(); resepti.setNimi("Tee");
+        assertEquals("Nimeä ei asetettu oikein", "Tee", resepti.getNimi());
+        resepti.setNimi("Kahvi");
+        assertEquals("Nimi ei vaihtunut toisella .setNimi() -kutsulla", "Kahvi", resepti.getNimi());
+    }
+    
+    /**
+     * Testaa nimen hakemista
+     */
+    @Test
+    public void testGetNimi() {
+        Resepti resepti = new Resepti(); resepti.setNimi("Tee");
+        assertEquals("Nimi on väärin", "Tee", resepti.getNimi());
+    }
+    
+    /**
+     * Testaa ohjeen asettamista
+     */
+    @Test
+    public void testSetOhje() {
+        Resepti resepti = new Resepti(); resepti.setOhje("Keita vesi.\nUita teepussia vedessa.");
+        assertEquals("Nimeä ei asetettu oikein", "Keita vesi.\nUita teepussia vedessa.", resepti.getOhje());
+        resepti.setOhje("Keita vesi.\nUita teepussia vedessa.\nNauti.");
+        assertEquals("Ohje ei vaihtunut toisella .setOhje() -kutsulla", "Keita vesi.\nUita teepussia vedessa.\nNauti.", resepti.getOhje());
+    }
+    
+    /**
+     * Testaa ohjeen hakemista
+     */
+    @Test
+    public void testGetOhje() {
+        Resepti resepti = new Resepti(); resepti.setOhje("Keita vesi.\nUita teepussia vedessa.");
+        assertEquals("Nimi on väärin", "Keita vesi.\nUita teepussia vedessa.", resepti.getOhje());
+    }
+    
+    /**
+     * Testaa id:n asettamista
+     */
+    @Test
+    public void testAsetaId() {
+        Resepti resepti1 = new Resepti(); int reseptin1Id = resepti1.asetaId();
+        assertEquals("Asettaminen ei palauttanut id:tä", reseptin1Id, resepti1.getId());
+        resepti1.asetaId();
+        assertEquals("Toinen asetaId() -kutsu vaihtoi id:tä", reseptin1Id, resepti1.getId());
+        Resepti resepti2 = new Resepti();
+        assertEquals("Seuraava asetettu id ei ollut yhtä suurempi kuin edellinen", resepti2.asetaId(), resepti1.getId() + 1);
+    }
+
+    /**
+     * Testaa parse() -metodia
+     */
+    @Test
+    public void testParse() {
+        Resepti resepti = new Resepti(); 
+        resepti.parse("3|Tee|Kuumenna vesi kiehuvaksi.§Uita teepussia vedessä.§Nauti.");
+        assertEquals("Väärä id", 3, resepti.getId());
+        assertEquals("Väärä nimi", "Tee", resepti.getNimi());
+        assertEquals("Väärä ohje", "Kuumenna vesi kiehuvaksi.\nUita teepussia vedessä.\nNauti.", resepti.getOhje());
+        assertFalse("Vääränlaisessa järjestyksessä oleva syöte toimi", resepti.parse("Tee|3|Kuumenna vesi kiehuvaksi.§Uita teepussia vedessä.§Nauti."));
+        assertFalse("Väärä määrä tietueita syötteessä toimi", resepti.parse("Tee3|Kuumenna vesi kiehuvaksi.§Uita teepussia vedessä.§Nauti."));
     }
 
  
